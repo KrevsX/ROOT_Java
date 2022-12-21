b7059206230064f34c959cb91504216338c596a5
KrevsX <elmrochino,yo@gmail.com>
2022-12-21 00:55:33 -0600

    update

--- src/JAVA_V/SextaEntrega/String/CadenaTexto.java
+++ src/JAVA_V/SextaEntrega/String/CadenaTexto.java
@@ -15,26 +15,40 @@ public class CadenaTexto {
     public static void main(String[] args) {
 
 
+        // Version 1
+        System.out.println("-----------------------");
+
+        System.out.println("Version desde Funcion");
+
+        String reversed = reverse("HOLA MUNDO - DESDE FUNCION");
+        System.out.println(reversed);
+
+
         System.out.println("-----------------------");
         String cadenaTexto = "HOLAAA MUNDO";
 
-        // Version 1
-        System.out.println("Version 1");
+        // Version 2
+        System.out.println("Version 2");
 
         for (int i = cadenaTexto.length() - 1; i >= 0; i--) {
             System.out.print(cadenaTexto.charAt(i));
         }
 
-        // Version 2
+        // Version 3
         System.out.println("\n-----------------------");
 
-        System.out.println("Version 2");
+        System.out.println("Version 3");
          for (int i = cadenaTexto.length() - 1; i >= 0; i--) {
             System.out.print(cadenaTexto.charAt(i));
             System.out.println();
          }
         System.out.println("-----------------------");
 
-
     }
+    public static String reverse(String texto) {
+        StringBuilder sb = new StringBuilder(texto);
+        sb.reverse();
+        return sb.toString();
+    }
+
 }
