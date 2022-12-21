258abd968a8f1c8d152c5025788ac8498f529434
KrevsX <elmrochino,yo@gmail.com>
2022-12-20 20:27:09 -0600

    update

--- src/JAVA_V/TipoDatosAvanzados/Arrays/Main.java
+++ src/JAVA_V/TipoDatosAvanzados/Arrays/Main.java
@@ -11,6 +11,12 @@ package JAVA_V.TipoDatosAvanzados.Arrays;
 public class Main {
     public static void main(String[] args) {
 
+        //Array1();
+        Array2();
+
+
+    }
+    public static void Array1(){
         int arrayDos[] = {1,2,3,4,5};
         int arrayUno[] = new int[5];
         arrayUno[0] = 1;
@@ -47,5 +53,45 @@ public class Main {
         }
 
         System.out.println(ultimoNombre);
+    }
+
+    public static void Array2(){
+
+        // Recorrer un Array Bidimensional
+
+        int arrayBidi[][] = new int [2][4];
+        arrayBidi[0][0] = 1;
+        arrayBidi[0][1] = 2;
+        arrayBidi[0][2] = 3;
+        arrayBidi[0][3] = 4;
+        arrayBidi[1][0] = 10;
+        arrayBidi[1][1] = 20;
+        arrayBidi[1][2] = 30;
+        arrayBidi[1][3] = 40;
+
+        // 2 filas x 4 columnas
+//
+//        int arrayBidi[][] = {
+//                {1,2,3,4},
+//                {10,20,30,40}
+//        };
+
+        for(int i = 0; i < arrayBidi.length; i++){
+            System.out.println("valor de i: " + i);
+            for(int j = 0; j < arrayBidi[i].length; j++){
+                System.out.println("Estoy en i = " + i + ", j = " + j);
+                System.out.println(arrayBidi[i][j]);
+            }
+        }
+
+//        int enteros = new int [100]; //100 2^32bits
+
+        String nombres [] = {"Pepe", "Juan"};
+        nombres[0] = "Victor";
+        nombres[1] = "Francisco";
+
+        for (String nombre : nombres){
+            System.out.println(nombre);
+        }
     }
 }
